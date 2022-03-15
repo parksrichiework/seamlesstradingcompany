1152da94b48420d9fd050edfed8475fde9dbfcbd
Richie Parks <parksrichiework@gmail.com>
2022-03-15 17:43:19 -0500

    updating folder files

---
+++ js/main.js
@@ -0,0 +1,21 @@
+     
+  
+
+  function multiplyThree(q,w,e){
+    return q * w * e
+  }
+
+console.log(multiplyThree(3*2*2))
+
+ 
+h1Holder.addEventListener('click', sum);
+    function sum(){
+        console.log(num + Number(inputedVal))
+    };
+
+
+inputHolder.addEventListener('click', log)
+
+    function log(){
+        console.log()
+    }
