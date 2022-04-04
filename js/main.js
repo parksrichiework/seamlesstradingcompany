990fe367d4c96899e831ac369818dea917862018
Richie Parks <parksrichiework@gmail.com>
2022-04-04 14:03:31 -0500

    updated html and css
    - added "ethical production" page (html and css)
    - added "request quote" pop up forms

--- js/main.js
+++ js/main.js
@@ -1,27 +1,49 @@
      
   
 
-  function multiplyThree(q,w,e){
-    return q * w * e
-  }
+//   function multiplyThree(q,w,e){
+//     return q * w * e
+//   }
 
-console.log(multiplyThree(3*2*2))
+// console.log(multiplyThree(3*2*2))
 
  
-h1Holder.addEventListener('click', sum);
-    function sum(){
-        console.log(num + Number(inputedVal))
-    };
+// h1Holder.addEventListener('click', sum);
+//     function sum(){
+//         console.log(num + Number(inputedVal))
+//     };
 
 
-inputHolder.addEventListener('click', log)
+// inputHolder.addEventListener('click', log)
 
-    function log(){
-        console.log()
-    }
+//     function log(){
+//         console.log()
+//     }
 
 
-var formInputs= document.querySelector('.input')
+// var formInputs= document.querySelector('.input')
 
-console.log(formInputs)
+// console.log(formInputs)
 
+// modal for pop-up form
+
+
+  function openForm() {
+        document.getElementById("popupForm").style.display = "block";
+      }
+      function closeForm() {
+        document.getElementById("popupForm").style.display = "none";
+      }
+
+var nameInput = document.getElementById('formContainer').value;
+
+console.log(nameInput)
+;
+
+function submitInfo(){
+    document.querySelectorAll('submit-info')
+}
+
+
+
+      //MODAL FOR POP UP FORM END
