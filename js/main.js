ca7ea34f2ead87fc885770d589a6670996ff2f88
Richie Parks <parksrichiework@gmail.com>
2022-04-25 13:23:39 -0500

    updated html and js
    js:
    - functions for: open, close and log info for requesting a sample

--- js/main.js
+++ js/main.js
@@ -1,73 +1,99 @@
-// *************************************modal for pop-up form**********************************************
 
 
-document.querySelector('#pricingRequest').addEventListener('click', openPricing)
-document.querySelector('#shippingRequest').addEventListener('click', openShipping)
+// OPEN PRICING AND SHIPPING REQUEST BUTTONS
+  document.querySelector('#pricingRequest').addEventListener('click', openPricing)
+  document.querySelector('#shippingRequest').addEventListener('click', openShipping)
+  document.querySelector('#sampleRequest').addEventListener('click', openSample)
 
 
-function openPricing(){
+  function openPricing(){
 
-  const form= document.querySelector('#popupFormPricing')
-      
-  form.style.display= "block"
-}
+    const form= document.querySelector('#popupFormPricing')
+        
+    form.style.display= "block"
+  }
 
-function openShipping(){
-  const form= document.querySelector('#popupFormShipping')
-  form.style.display= "block"
-}
+  function openShipping(){
+    const form= document.querySelector('#popupFormShipping')
+    form.style.display= "block"
+  }
+
+  function openSample(){
+    const form= document.querySelector('#popupFormSample')
+    form.style.display= 'block'
+  }
+
+// CLOSE PRICING AND SHIPPING REQUEST BUTTONS
+  document.querySelector('#closeBtn').addEventListener('click', close)
+  document.querySelector('#closeBtnShipping').addEventListener('click', closeShipping)
+  document.querySelector('#closeBtnSample').addEventListener('click', closeSample)
 
 
-document.querySelector('#closeBtn').addEventListener('click', close)
-document.querySelector('#closeBtnShipping').addEventListener('click', closeShipping)
+  function close(){
+    const formPricing= document.querySelector('#popupFormPricing')
+    formPricing.style.display= "none"
+    
+  }
 
+  function closeShipping(){
+    const form= document.querySelector('#popupFormShipping')
+    form.style.display= "none"
+  }
 
-function close(){
-  const formPricing= document.querySelector('#popupFormPricing')
-  formPricing.style.display= "none"
-  
-}
-
-function closeShipping(){
-  const form= document.querySelector('#popupFormShipping')
-  form.style.display= "none"
-}
+  function closeSample(){
+    const form= document.querySelector('#popupFormSample')
+    form.style.display= "none"
+  }
 
     
+// LOGGING THE INFORMATION FROM THE PRICING AND SHIPPING REQUEST BUTTONS
+  document.querySelector('#submitBtnPricing').addEventListener('click', logPricing)
 
-document.querySelector('#submitBtnPricing').addEventListener('click', log)
+  function logPricing(){
+    const name= document.querySelector('#namePricing').value
+    const company= document.querySelector('#company-namePricing').value
+    const email= document.querySelector('#emailPricing').value
 
-function log(){
-  const name= document.querySelector('#namePricing').value
-  const company= document.querySelector('#company-namePricing').value
-  const email= document.querySelector('#emailPricing').value
+    console.log(`Pricing request from ${name}, ${company}, ${email}`)
+    alert("Your Pricing Quote information has been submitted!")
 
-  console.log("Pricing quote requested by: " + name + ", " + company + ", " + email)
-  alert("Your Pricing Quote information has been submitted!")
+    return document.querySelector('#pricingRequest').innerHTML= "<p>Submit Another?</p>"
+    }
 
-  return document.querySelector('#pricingRequest').innerHTML= "<p>Submit Another?</p>"
+
+  document.querySelector('#submitBtnShipping').addEventListener('click', logShipping)
+
+  function logShipping(){
+    const name= document.querySelector('#nameShipping').value
+    const company= document.querySelector('#company-nameShipping').value
+    const email= document.querySelector('#emailShipping').value
+
+    console.log(`Shipping request from ${name}, ${company}, ${email}`)
+    alert("Your Shipping Quote information has been submitted!")
+
+    return document.querySelector('#shippingRequest').innerHTML= "<p>Submit Another?</p>"
+    }
+
+      document.querySelector('#submitBtnSample').addEventListener('click', logSample)
+
+  function logSample(){
+    const name= document.querySelector('#nameSample').value
+    const company= document.querySelector('#companySample').value
+    const email= document.querySelector('#emailSample').value
+
+    console.log(`Sample request from ${name}, ${company}, ${email}`)
+    alert("Your Sample request information has been submitted!")
+
+    return document.querySelector('#sampleRequest').innerHTML= "Submit Another?"
   }
 
 
-document.querySelector('#submitBtnShipping').addEventListener('click', log)
-
-function log(){
-  const name= document.querySelector('#nameShipping').value
-  const company= document.querySelector('#company-nameShipping').value
-  const email= document.querySelector('#emailShipping').value
-
-  console.log("Shipping quote requested by: " + name + ", " + company + ", " + email)
-  alert("Your Shipping Quote information has been submitted!")
-
-  return document.querySelector('#shippingRequest').innerHTML= "<p>Submit Another?</p>"
-  }
 
 
 
 
-  // const synth = window.speechSynthesis;
 
-  // let yellThis = new SpeechSynthesisUtterance(yellText);
+
 
 
 
